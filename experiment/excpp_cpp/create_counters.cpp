058b0186d8905686ad02780e2c8babd9d56285a5
Ross <rdaly525@stanford.edu>
2018-05-07 11:57:23 -0700

    moved to experiment folder

---
+++ experiment/excpp_cpp/create_counters.cpp
@@ -0,0 +1,48 @@
+#include "coreir.h"
+#include "rosslib_dec.h"
+
+using namespace std;
+using namespace CoreIR;
+
+//Making a slow counter.
+
+int main() {
+  
+  Context* c = newContext();
+  Namespace* g = c->getGlobal();
+
+  //This is the "header" file include
+  CoreIRLoadLibrary_rosslib(c);
+  
+  // Define the slow counter
+  Type* topType = c->Record({
+    {"clk",c->Named("coreir.clkIn")},
+    {"out",c->Bit()->Arr(16)}
+  });
+
+  Module* top = g->newModuleDecl("top",topType);
+  ModuleDef* def = top->newModuleDef();
+  {
+    def->addInstance("count_fast","rosslib.counter",{{"width",Const::make(c,23)},{"has_en",Const::make(c,false)}});
+    def->addInstance("count_slow","rosslib.counter",{{"width",Const::make(c,16)},{"has_en",Const::make(c,true)}});
+    def->addInstance("eq0","coreir.eq",{{"width",Const::make(c,23)}});
+    def->addInstance("c0","coreir.const",{{"width",Const::make(c,23)}},{{"value",Const::make(c,23,0)}});
+    
+    def->connect("self.clk","count_fast.clk");
+    def->connect("self.clk","count_slow.clk");
+    def->connect("count_slow.out","self.out");
+    def->connect("c0.out","eq0.in0");
+    def->connect("count_fast.out","eq0.in1");
+    def->connect("eq0.out","count_slow.en");
+  }
+  top->setDef(def);
+  c->setTop(top);
+  
+  c->runPasses({"cullgraph"});
+  if (!saveToFile(c,"counters.json")) {
+    cout << "Save to file Failed!!!";
+    exit(1);
+  }
+
+  deleteContext(c);
+}
---
+++ experiment/excpp_cpp/rosslib_dec.cpp
@@ -0,0 +1,38 @@
+#include "rosslib_dec.h"
+
+using namespace CoreIR;
+
+COREIR_GEN_C_API_DEFINITION_FOR_LIBRARY(rosslib);
+
+
+Namespace* CoreIRLoadLibrary_rosslib(Context* c) {
+  Namespace* rosslib = c->newNamespace("rosslib");
+
+  Params counterParams({
+    {"width", c->Int()},
+    {"has_en", c->Bool()},
+  });
+
+  TypeGen* counterType = rosslib->newTypeGen(
+    "counterType", //Name of TypeGen
+    counterParams, //Parameters to be passed into TypeGen
+    [](Context* c, Values genargs) {
+      uint width = genargs.at("width")->get<int>();
+      bool has_en = genargs.at("has_en")->get<bool>();
+      RecordParams ioParams({
+        {"clk", c->Named("coreir.clkIn")},
+        {"in", c->BitIn()->Arr(width)},
+        {"out", c->Bit()->Arr(width)}
+      });
+      if (has_en) {
+        ioParams.push_back({"en",c->BitIn()});
+      }
+      return c->Record(ioParams);
+    }
+  );
+
+  rosslib->newGeneratorDecl("counter", counterType, counterParams);
+  return rosslib;
+}
+
+COREIR_GEN_EXTERNAL_API_FOR_LIBRARY(rosslib)
